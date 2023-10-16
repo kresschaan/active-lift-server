dbc1e8ffca1e3abb580a9940bfd3509bdee048e9
Christian Cho <christianallancho@gmail.com>
2023-10-16 10:59:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,16 +15,17 @@ const corsOptions = {
     optionSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/", routesHandler);
-
 const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
 mongoose
     .connect(mongoKeys, dbOptions)
-    .then(() => console.log("DB Connected!"))
+    .then(() => {
+        app.use(cors(corsOptions));
+        app.use(express.json());
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use("/", routesHandler);
+    })
     .catch((err) => console.log(err));
 
 app.listen(port, () => {
